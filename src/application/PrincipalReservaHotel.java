305e7f03d2de0eccef7748c98a983bcdf5d53551
Luiz Augusto <luizaugustodeandrades@gmail.com>
2022-08-16 14:09:56 -0300

    Versao de solucao de tratamentos de excecoes muito ruim

---
+++ src/application/PrincipalReservaHotel.java
@@ -0,0 +1,56 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import model.entities.Reserva;
+
+public class PrincipalReservaHotel {
+
+	public static void main(String[] args) throws ParseException {
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		char resp;
+
+		do {
+			System.out.print("NUMERO DO QUARTO PARA RESERVA: ");
+			int number = sc.nextInt();
+			System.out.print("CHECK-IN DATE(DD/MM/YYYY): ");
+			Date checkIn = sdf.parse(sc.next());
+			System.out.print("CHECK-OUT DATE(DD/MM/YYYY): ");
+			Date checkOut = sdf.parse(sc.next());
+
+			if (!checkOut.after(checkIn)) {
+				System.out.println("ERRO NA RESERVA, A DATA DE CHECK-OUT DEVE SER APÓS A DATA DE CHECK-IN!");
+			} else {
+				Reserva reserva = new Reserva(number, checkIn, checkOut);
+				System.out.println("RESERVA: " + reserva);
+
+				System.out.println();
+				System.out.println("ENTRE PARA ATUALIZAR AS DATAS DA RESERVA:");
+				System.out.print("CHECK-IN DATE(DD/MM/YYYY): ");
+				checkIn = sdf.parse(sc.next());
+				System.out.print("CHECK-OUT DATE(DD/MM/YYYY): ");
+				checkOut = sdf.parse(sc.next());
+
+				Date agora = new Date();
+
+				if (checkIn.before(agora) || checkOut.before(agora)) {
+					System.out.println("ERRO NA RESERVA, AS DATAS DE RESERVA TEM QUE SER DATAS FUTURAS!");
+				} else if (!checkOut.after(checkIn)) {
+					System.out.println("ERRO NA RESERVA, A DATA DE CHECK-OUT DEVE SER APÓS A DATA DE CHECK-IN!");
+				} else {
+					reserva.updateDates(checkIn, checkOut);
+					System.out.println("RESERVA: " + reserva);
+				}
+			}
+			System.out.println();
+			System.out.print("DESEJA REINICIAR O SISTEMA: ");
+			resp = sc.next().charAt(0);
+		} while (resp == 's');
+		sc.close();
+	}
+
+}
